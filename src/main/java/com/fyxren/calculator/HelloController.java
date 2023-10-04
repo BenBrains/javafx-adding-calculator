5fee4277df636239c456ee97bb29110559d7c9c6
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-10-04 11:25:48 +0200

    🤯 `Add` subtract functionality

--- src/main/java/com/fyxren/calculator/HelloController.java
+++ src/main/java/com/fyxren/calculator/HelloController.java
@@ -23,4 +23,15 @@ public class HelloController {
             output.setText("Error - Error when parsing floats");
         }
     }
+
+    @FXML
+    protected void onSubtract() {
+        try {
+            float val1 = Float.parseFloat(input1.getText().replaceAll(",", "."));
+            float val2 = Float.parseFloat(input2.getText().replaceAll(",", "."));
+            output.setText(String.valueOf(val1 - val2));
+        } catch (Exception e) {
+            output.setText("Error - Error when parsing floats");
+        }
+    }
 }
